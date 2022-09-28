005e8fec542898abc7f767e4963b27abd20456ef
Michael Fehrer <mtfehrer@gmail.com>
2022-09-27 21:11:06 -0400

    Multiple changes

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -4,18 +4,32 @@ type Props = {
     title: string;
     description: string;
     image: string;
+    alt: string;
 };
 
-const Project: React.FC<Props> = ({ title, description, image }: Props) => {
+const Project: React.FC<Props> = ({
+    title,
+    description,
+    image,
+    alt,
+}: Props) => {
+    const animationElements = document.querySelectorAll("project-container");
+    console.log(animationElements);
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            entry.target.classList.toggle("show", entry.isIntersecting);
+        });
+    });
+
+    animationElements.forEach((e) => {
+        observer.observe(e);
+    });
     return (
         <div className="project-container">
-            <h1>{title}</h1>
-            <img
-                className="project-image"
-                src={image}
-                alt="tower defense game"
-            ></img>
-            <span>{description}</span>
+            <h1 className="title">{title}</h1>
+            <img className="project-image" src={image} alt={alt}></img>
+            <span className="description">{description}</span>
         </div>
     );
 };
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,19 +7,49 @@ const Projects: React.FC = () => {
             <h1 className="projects-title">Projects</h1>
             <Project
                 title="Tower Defense Game"
-                description="Lorem ipsum"
-                image="tower defense game.png"
+                description="Built with React and Typescript"
+                image="tower defense.gif"
+                alt="tower defense game"
             />
             <Project
-                title="Tower Defense Game"
-                description="Lorem ipsum"
-                image="tower defense game.png"
+                title="2D Shooter Game"
+                description="Developed with the pygame engine in python"
+                image="shooter game2.gif"
+                alt="2D Shooter Game"
             />
             <Project
-                title="Tower Defense Game"
-                description="Lorem ipsum"
-                image="tower defense game.png"
+                title="Forum Website"
+                description="Built with the MERN stack"
+                image="forum site.png"
+                alt="forum website"
             />
+            <Project
+                title="Pokemon Battle"
+                description="Created using the curses module in python."
+                image="simple pokemon.gif"
+                alt="pokemon game"
+            />
+            <Project
+                title="FPS Game"
+                description="Coded using the ursina module in python."
+                image="fps game.gif"
+                alt="fps game"
+            />
+            <Project
+                title="Sniper Game"
+                description="Developed with the pygame engine in python"
+                image="sniper game.gif"
+                alt="sniper game"
+            />
+            <Project
+                title="Maze Game"
+                description="Programmed with the pygame engine in python"
+                image="maze game.gif"
+                alt="maze game"
+            />
+            <h1>
+                And more on my <a href="https://github.com/mtfehrer">github</a>
+            </h1>
         </div>
     );
 };
