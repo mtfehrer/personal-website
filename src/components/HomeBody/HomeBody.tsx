7bdf7d57b69dd9bd89bb5b9bcf4b5a074a79f6bb
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2024-08-24 18:11:58 -0400

    test deployment

--- src/components/HomeBody/HomeBody.tsx
+++ src/components/HomeBody/HomeBody.tsx
@@ -9,18 +9,15 @@ const HomeBody: React.FC = () => {
             <div className="home-body">
                 <div className="intro-container fade-in">
                     <div>
-                        <h1 className="intro-title">Hey, I'm Michael.</h1>
+                        <h1 className="intro-title">Hey, I'm Michael. Test</h1>
                         <span className="intro-text">
-                            I'm an avid coder. I've been programming and
-                            developing software for years. I'm primarily focused
-                            with building websites and backend systems.
+                            I'm an avid coder. I've been programming and developing software for
+                            years. I'm primarily focused with building websites and backend systems.
                         </span>
                     </div>
                     <img
                         className="profile-image"
-                        src={
-                            process.env.PUBLIC_URL + "/images/profile-photo.jpg"
-                        }
+                        src={process.env.PUBLIC_URL + "/images/profile-photo.jpg"}
                         alt="profile"
                     ></img>
                 </div>
